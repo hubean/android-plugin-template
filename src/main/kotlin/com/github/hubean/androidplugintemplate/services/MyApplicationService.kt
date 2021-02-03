02a889cbad5406f094d7613823df3897dc31f85a
GitHub Action <action@github.com>
2021-02-03 12:38:04 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/hubean/androidplugintemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.hubean.androidplugintemplate.services
+
+import com.github.hubean.androidplugintemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/hubean/androidplugintemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/hubean/androidplugintemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.hubean.androidplugintemplate.services
 
+import com.github.hubean.androidplugintemplate.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
